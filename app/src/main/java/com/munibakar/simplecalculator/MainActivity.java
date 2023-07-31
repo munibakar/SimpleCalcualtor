b985002baa201c30d9c06cac8812de00adee3a99
munibakar <mnakar54@gmail.com>
2023-07-31 12:36:32 +0300

    SimpleCalculator
    Simple level calculator with android studio

---
+++ app/src/main/java/com/munibakar/simplecalculator/MainActivity.java
@@ -0,0 +1,90 @@
+package com.munibakar.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText number1Text;
+    EditText number2Text;
+    TextView resultText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        number1Text = findViewById(R.id.number1Text);
+        number2Text = findViewById(R.id.number2Text);
+        resultText = findViewById(R.id.ResultText);
+    }
+
+    public void sum(View view){
+
+        if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
+            resultText.setText("Invalid Value. Please enter the number!");
+        }else {
+            int number1 = Integer.parseInt(number1Text.getText().toString());
+            int number2 = Integer.parseInt(number2Text.getText().toString());
+
+            int result = number1 + number2;
+
+            resultText.setText("Result: " + result);
+        }
+
+    }
+
+    public void deduct(View view){
+        if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
+            resultText.setText("Invalid Value. Please enter the number!");
+        }else {
+            int number1 = Integer.parseInt(number1Text.getText().toString());
+            int number2 = Integer.parseInt(number2Text.getText().toString());
+
+            int result = number1 - number2;
+
+            resultText.setText("Result: " + result);
+        }
+
+    }
+
+    public void multiply(View view){
+        if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
+            resultText.setText("Invalid Value. Please enter the number!");
+        }else {
+            int number1 = Integer.parseInt(number1Text.getText().toString());
+            int number2 = Integer.parseInt(number2Text.getText().toString());
+
+            int result = number1 * number2;
+
+            resultText.setText("Result: " + result);
+
+        }
+
+
+    }
+
+    public void divide(View view){
+        if (number1Text.getText().toString().matches("") || number2Text.getText().toString().matches("")){
+            resultText.setText("Invalid Value. Please enter the number!");
+        }else {
+            if (Integer.parseInt(number2Text.getText().toString()) == 0){
+                resultText.setText("Invalid Value!");
+            }else{
+                int number1 = Integer.parseInt(number1Text.getText().toString());
+                int number2 = Integer.parseInt(number2Text.getText().toString());
+
+                int result = number1 / number2;
+
+                resultText.setText("Result: " + result);
+            }
+
+
+        }
+
+
+    }
+}
